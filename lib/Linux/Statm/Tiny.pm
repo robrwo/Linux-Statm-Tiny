45b1f349c86471cf6583a207a399002d6cbe14a5
Robert Rothenberg <rrwo@thermeon.com>
2014-12-22 13:26:58 +0000

    Initial prototype

---
+++ lib/Linux/Statm/Tiny.pm
@@ -0,0 +1,148 @@
+package Linux::Statm::Tiny;
+
+use Moo;
+
+use Types::Standard qw/ ArrayRef Int /;
+
+{
+    $Linux::Statm::Tiny::VERSION = '0.0100'
+}
+
+=head1 NAME
+
+Linux::Statm::Tiny - simple access to Linux /proc/../statm
+
+=for readme plugin version
+
+=head1 SYNOPSIS
+
+  use Linux::Statm::Tiny;
+
+  my $stats = Linux::Statm::Tiny->new( pid => $$ );
+
+  my $size = $stats->size;
+
+=begin :readme
+
+=head1 INSTALLATION
+
+See
+L<How to install CPAN modules|http://www.cpan.org/modules/INSTALL.html>.
+
+=for readme plugin requires heading-level=2 title="Required Modules"
+
+=for readme plugin changes
+
+=end :readme
+
+=head1 DESCRIPTION
+
+This class returns the Linux memory stats from F</proc/$pid/statm>.
+
+=for readme stop
+
+=head1 ATTRIBUTES
+
+=head2 C<pid>
+
+The PID to obtain stats for. If omitted, it uses the current PID from
+C<$$>.
+
+=cut
+
+has pid => (
+    is      => 'lazy',
+    isa     => Int,
+    default => $$,
+    );
+
+=head2 C<statm>
+
+The raw array reference of values.
+
+=cut
+
+has statm => (
+    is       => 'lazy',
+    isa      => ArrayRef[Int],
+    init_arg => undef,
+    );
+
+sub _build_statm {
+    my ($self) = @_;
+    my $pid = $self->pid;
+    open my $fh, '<', "/proc/${pid}/statm"
+        or die "Unable to open /proc/${pid}/statm";
+    chomp(my $raw = <$fh>);
+    close $fh;
+    [ split / /, $raw ];
+    }
+
+=head2 C<size>
+
+Total program size, in pages.
+
+=head2 C<resident>
+
+Resident set size (RSS), in pages.
+
+=head2 C<share>
+
+Shared pages.
+
+=head2 C<text>
+
+Text (code).
+
+=head2 C<lib>
+
+Library (unused in Linux 2.6).
+
+=head2 C<data>
+
+Data + Stack.
+
+=head2 C<dt>
+
+Dirty pages (unused in Linux 2.6).
+
+=cut
+
+my %stats = (
+    size     => 0,
+    resident => 1,
+    share    => 2,
+    text     => 3,
+    lib      => 4,
+    data     => 5,
+    dt       => 6,
+    );
+
+foreach my $attr (keys %stats) {
+    has $attr => (
+        is       => 'lazy',
+        isa      => Int,
+        default  => sub { shift->statm->[$stats{$attr}] },
+        init_arg => undef,
+        );
+    }
+
+=for readme continue
+
+=head1 SEE ALSO
+
+L<proc(5)>.
+
+=head1 AUTHOR
+
+Robert Rothenberg C<< rrwo@thermeon.com >>
+
+=head1 COPYRIGHT
+
+Copyright 2014, Thermeon Worldwide, PLC.
+
+=cut
+
+use namespace::sweep;
+
+1;
---
+++ t/10-basic.t
@@ -0,0 +1,24 @@
+use Test::More;
+use common::sense;
+
+use_ok 'Linux::Statm::Tiny';
+
+ok my $stat = Linux::Statm::Tiny->new(), 'new';
+
+my %stats = (
+    size     => 0,
+    resident => 1,
+    share    => 2,
+    text     => 3,
+    data     => 5,
+    );
+
+note( explain $stat->statm );
+
+foreach my $key (keys %stats) {
+    can_ok $stat, $key;
+    note "${key} = " . $stat->$key;
+    is $stat->$key, $stat->statm->[$stats{$key}], $key;
+    }
+
+done_testing;
