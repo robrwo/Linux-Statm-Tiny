8c4d79071721fcd706b44d76ec7e1fcde3b565a4
Robert Rothenberg <rrwo@cpan.org>
2022-04-04 16:34:27 +0100

    Commit Changes and bump $VERSION

--- lib/Linux/Statm/Tiny.pm
+++ lib/Linux/Statm/Tiny.pm
@@ -11,7 +11,7 @@ use Types::Standard qw/ ArrayRef Int /;
 
 use constant page_size => POSIX::sysconf POSIX::_SC_PAGESIZE;
 
-our $VERSION = '0.0602';
+our $VERSION = '0.0603';
 
 # ABSTRACT: simple access to Linux /proc/../statm
 
